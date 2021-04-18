9537f3a653c0cb5b400e05c01b917cead11e71b5
Lalicc <82774913+Lalicc@users.noreply.github.com>
2021-04-18 20:25:32 +0200

    kitica
    duvaj jaja

--- js/script.js
+++ js/script.js
@@ -17,31 +17,23 @@ function openInNewTab(url) {
 
 //!SMOOTHSCROLL
 $(document).ready(function(){
-  // Add smooth scrolling to all links
   $("a").on('click', function(event) {
 
-    // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
-      // Prevent default anchor click behavior
       event.preventDefault();
-      // Store hash
       var hash = this.hash;
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
         scrollTop: $(hash).offset().top
       }, 800, function(){
-        // Add hash (#) to URL when done scrolling (default click behavior)
         window.location.hash = hash;
       });
-    } // End if
+    } 
   });
 });
 
 //SCROLL ON TOP BUTTON
 var mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
