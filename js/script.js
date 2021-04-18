3b34963b9f8a3529d6c799b06b9d663905d08c06
Lalicc <82774913+Lalicc@users.noreply.github.com>
2021-04-18 20:00:45 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,57 @@
+//TRANSPARENT NAVBAR
+$(window).scroll(function () {
+  if ($(window).scrollTop() >= 50) {
+    $('.navbar').addClass('js-navbar-bg')
+    $('.navbar').removeClass('js-transparent')
+  } else {
+    $('.navbar').removeClass('js-navbar-bg')
+    $('.navbar').addClass('js-transparent')
+  }
+})
+//!TRANSPARENT NAVBAR
+
+function openInNewTab(url) {
+  var win = window.open(url, '_blank');
+  win.focus();
+}
+
+//!SMOOTHSCROLL
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+      // Store hash
+      var hash = this.hash;
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
+
+//SCROLL ON TOP BUTTON
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
